b4fe72f173f9cbe192b0013131456a301fb29292
ch_biswal <chandrikabiswal1@gmail.com>
2020-06-06 12:48:26 +0530

    react-app-using-twitter-API
    Fetching tweets  using twitter API.

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'Q1i7cJPRvND6vvCuVmAWBzoEc',
+    consumer_secret: 'e8LksTCsAwpiRRCZbbLLAZ4IOdnyFmiRWM2qhMJLrAxat92NcO',
+    access_token_key: '1268763623322644480-SpNfMHS49FWMbRZUoBW8mG9WDSLsxo',
+    access_token_secret: '3VXk3bsS52Ndz6exzW7OmNDyPuY2fCDoBh9qZhoSsEe6M'
+  }
+}
